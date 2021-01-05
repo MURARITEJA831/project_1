14259ca14a9855c20283d14b2a048f70c4746622
MURARITEJA831 <murariteja831@gmail.com>
2021-01-05 16:57:28 +0530

    just added some bootstrap

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author':'Murari',
+        'title' :'Blog_post 1',
+        'content': 'first post ',
+        'date_posted':'jan 5 2021'
+    },
+    {
+        'author':'teja',
+        'title':'blog_post 2',
+        'content':'2nd post',
+        'date_posted':'jan 5 2021'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog-Home</h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>About-page</h1>')
+    return render(request,'blog/about.html',{'title':'About'})
