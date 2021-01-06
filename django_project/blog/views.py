aff933a0615e20caeee8fed363bc8b8a492e87fd
MURARITEJA831 <murariteja831@gmail.com>
2021-01-06 13:42:41 +0530

    created models and created registration form and crispy added for styling the registration form

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author':'Murari',
-        'title' :'Blog_post 1',
-        'content': 'first post ',
-        'date_posted':'jan 5 2021'
-    },
-    {
-        'author':'teja',
-        'title':'blog_post 2',
-        'content':'2nd post',
-        'date_posted':'jan 5 2021'
-    }
-]
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
